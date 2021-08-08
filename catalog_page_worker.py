18bc91000ac288b0d38f9357def1edf6ae3a54a8
Jim Shi <shiyilin2008@gmail.com>
2021-08-07 17:42:53 -0700

    Adds the catalog page worker and its test.

---
+++ catalog_page_worker.py
@@ -0,0 +1,70 @@
+"""
+Catalog page retrieval thread.
+"""
+
+import typing
+import time
+import random
+from queue import Queue
+import urllib.parse
+
+import downloader
+import html_parser
+
+
+SERVER_OVERLOADED_WAIT_INTERVAL_RANGE: typing.Final = (60.0, 120.0)
+"""
+If the catalog page thread encounters a server overload event, a random float number will be chosen between this range for the catalog page thread to sleep.
+"""
+
+PAGE_RETRIEVAL_INTERVAL_RANGE: typing.Final = (30.0, 60.0)
+"""
+A random float number will be chosen between this range for the (sleep) interval between catalog page requests.
+"""
+
+PAGE_RETRIEVAL_COMPLETE_PLACEHOLDER_URL: typing.Final = "complete_placeholder_url"
+"""
+The catalog page thread pushes this item into the queue once all catalog pages have been retrieved.
+
+The image detail page thread exits once it encounters this item form the queue.
+"""
+
+
+def catalog_page_worker(image_detail_page_urls_queue: Queue, start_url: str):
+    current_url = start_url
+    referer = None
+
+    while True:
+        print(f"Parsing catalog page `{current_url}` with referer `{referer}`.")
+
+        html_str = downloader.request_html(current_url, referer)
+
+        if html_parser.is_server_overloaded(html_str):
+            # Server overloaded -> Sleep for a long time.
+            sleep_duration = random.uniform(SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[0], SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[1])
+            print(f"Server is overloaded. Sleeping for {sleep_duration:.2f} seconds before trying again.")
+            time.sleep(sleep_duration)
+            continue
+
+        next_page_url, image_detail_page_urls = html_parser.parse_catalog_page(html_str)
+
+        # Add detail page URLs to queue.
+        for image_detail_page_url in image_detail_page_urls:
+            image_detail_page_urls_queue.put(image_detail_page_url)
+
+        print(f"Parsed {len(image_detail_page_urls)} URLs from `{current_url}`.")
+
+        if next_page_url:
+            # This is not the last page.
+            referer = current_url
+            current_url = urllib.parse.urljoin(current_url, next_page_url)    # `next_page_url` is usually relative: Need to join with current URL.
+
+            # Sleep between pages.
+            sleep_duration = random.uniform(PAGE_RETRIEVAL_INTERVAL_RANGE[0], PAGE_RETRIEVAL_INTERVAL_RANGE[1])
+            print(f"Sleeping for {sleep_duration:.2f} seconds before parsing the next catalog page.")
+            time.sleep(sleep_duration)
+
+        else:
+            # This is the last page
+            print(f"Finished parsing all catalog pages!")
+            break
---
+++ test_catalog_page_worker.py
@@ -0,0 +1,31 @@
+import unittest
+from queue import Queue
+
+from catalog_page_worker import catalog_page_worker
+
+
+class CatalogPageWorkerTestCase (unittest.TestCase):
+    def test_disguise_shirt(self):
+        """
+        This particular search has just 2 pages and should be fast to perform.
+        """
+        start_url = "https://safebooru.org/index.php?page=post&s=list&tags=disguise+shirt"
+
+        image_detail_page_urls_queue = Queue()
+        catalog_page_worker(image_detail_page_urls_queue, start_url)
+
+        self.assertGreater(image_detail_page_urls_queue.qsize(), 40)
+        self.assertLess(image_detail_page_urls_queue.qsize(), 80)    # NOTE: At the time of writing this test, this particular search has 47 results in total. As images get added over time, please re-perform the search to check if an increase in numbers is needed.
+
+        while not image_detail_page_urls_queue.empty():
+            image_detail_page_url = image_detail_page_urls_queue.get()
+
+            self.assertIsInstance(image_detail_page_url, str)
+
+            # Example: index.php?page=post&s=view&id=3579606
+            self.assertIn("?", image_detail_page_url)
+            self.assertIn("id=", image_detail_page_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
