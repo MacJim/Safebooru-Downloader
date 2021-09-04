1c049240407d1eab19fb9e86e1f9a511002ab875
Jim Shi <shiyilin2008@gmail.com>
2021-09-04 01:13:42 -0700

    [Minor] Catalog page worker: Reduces indentation.

--- catalog_page_worker.py
+++ catalog_page_worker.py
@@ -24,42 +24,52 @@ A random float number will be chosen between this range for the (sleep) interval
 
 
 def catalog_page_worker(image_detail_page_urls_queue: Queue, catalog_page_urls: typing.List[str]):
-    for start_url in catalog_page_urls:
-        current_url = start_url
-        referer = None
+    current_urls_and_referers: typing.List[typing.Tuple[str, typing.Optional[str]]] = [(url, None) for url in reversed(catalog_page_urls)]
+    """
+    A list of catalog page URLs and corresponding referers.
+    
+    Highest priority items are on the back (to ease `list.pop` behavior).
+    """
 
-        while True:
-            print(f"Parsing catalog page `{current_url}` with referer `{referer}`.")
+    while current_urls_and_referers:
+        current_url, referer = current_urls_and_referers[-1]
 
-            html_str = downloader.request_html(current_url, referer)
+        print(f"Parsing catalog page `{current_url}` with referer `{referer}`.")
 
-            if html_parser.is_server_overloaded(html_str):
-                # Server overloaded -> Sleep for a long time.
-                sleep_duration = random.uniform(SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[0], SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[1])
-                print(f"Server is overloaded. Sleeping for {sleep_duration:.2f} seconds before trying again.")
-                time.sleep(sleep_duration)
-                continue
+        html_str = downloader.request_html(current_url, referer)
 
-            next_page_url, image_detail_page_urls = html_parser.parse_catalog_page(html_str)
+        if html_parser.is_server_overloaded(html_str):
+            # Server overloaded -> Sleep for a long time.
+            sleep_duration = random.uniform(SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[0], SERVER_OVERLOADED_WAIT_INTERVAL_RANGE[1])
+            print(f"Server is overloaded. Sleeping for {sleep_duration:.2f} seconds before trying again.")
+            time.sleep(sleep_duration)
+            continue
+        else:
+            # Remove the current URL if the server isn't overloaded.
+            current_urls_and_referers.pop()
 
-            # Add detail page URLs and current URL (used as referer) to queue.
-            for image_detail_page_url in image_detail_page_urls:
-                image_detail_page_url = urllib.parse.urljoin(current_url, image_detail_page_url)
-                image_detail_page_urls_queue.put((image_detail_page_url, current_url))
+        # Parse the current catalog page.
+        next_page_url, image_detail_page_urls = html_parser.parse_catalog_page(html_str)
 
-            print(f"Parsed {len(image_detail_page_urls)} URLs from `{current_url}`.")
+        # Add parsed detail page URLs and current URL (used as referer) to queue.
+        for image_detail_page_url in image_detail_page_urls:
+            image_detail_page_url = urllib.parse.urljoin(current_url, image_detail_page_url)
+            image_detail_page_urls_queue.put((image_detail_page_url, current_url))
 
-            if next_page_url:
-                # This is not the last page.
-                referer = current_url
-                current_url = urllib.parse.urljoin(current_url, next_page_url)    # `next_page_url` is usually relative: Need to join with current URL.
+        print(f"Parsed {len(image_detail_page_urls)} URLs from `{current_url}`.")
 
-                # Sleep between pages.
-                sleep_duration = random.uniform(PAGE_RETRIEVAL_INTERVAL_RANGE[0], PAGE_RETRIEVAL_INTERVAL_RANGE[1])
-                print(f"Sleeping for {sleep_duration:.2f} seconds before parsing the next catalog page.")
-                time.sleep(sleep_duration)
+        # Add next catalog page URL to `current_urls_and_referers` if this is not the last page.
+        if next_page_url:
+            # This is not the last page.
+            next_url = urllib.parse.urljoin(current_url, next_page_url)    # `next_page_url` is usually relative: Need to join with current URL.
+            next_referer = current_url
 
-            else:
-                # This is the last page
-                print(f"Finished parsing all catalog pages!")
-                break
+            current_urls_and_referers.append((next_url, next_referer))
+
+        # Sleep between pages (if there are still pages remaining).
+        if current_urls_and_referers:
+            sleep_duration = random.uniform(PAGE_RETRIEVAL_INTERVAL_RANGE[0], PAGE_RETRIEVAL_INTERVAL_RANGE[1])
+            print(f"Sleeping for {sleep_duration:.2f} seconds before parsing the next catalog page.")
+            time.sleep(sleep_duration)
+
+    print(f"Finished parsing all catalog pages!")
